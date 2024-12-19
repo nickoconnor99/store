18b46473ce15d7da458be56766d1ef3d92bfea53
Paul Allen <nickoconnor@gmail.com>
2024-12-19 21:09:29 +0000

    updated hero page

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -7,13 +7,11 @@ function Hero() {
     <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
       <div>
         <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
-          We are changing the way people shop
+          Pow Pow Balloons
         </h1>
         <p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque et
-          voluptas saepe in quae voluptate, laborum maiores possimus illum
-          reprehenderit aut delectus veniam cum perferendis unde sint doloremque
-          non nam.
+          🎈PowPow balloons by Paulina 🎈Any occasion, big or small 🎈 Limerck
+          and surrounding areas 🎈 DM for availability
         </p>
         <Button asChild size="lg" className="mt-10">
           <Link href="/products">Our Products</Link>
