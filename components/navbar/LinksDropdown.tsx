0c06d559b08b20b6f014eba5a4f8815ccf0adda9
Paul Allen <nickoconnor@gmail.com>
2025-01-12 22:50:32 +0000

    add restrict admin routes

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -12,8 +12,12 @@ import { links } from "@/utils/links";
 import UserIcon from "./UserIcon";
 import SignOutLink from "./SignOutLink";
 import { SignInButton, SignUpButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 
 function LinksDropdown() {
+  const { userId } = auth();
+  const isAdmin = userId === process.env.ADMIN_USER_ID;
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -40,6 +44,8 @@ function LinksDropdown() {
         {/* SignedIn comp comes from Clerk */}
         <SignedIn>
           {links.map((link) => {
+            if (link.label === "dashboard" && !isAdmin) return null;
+
             return (
               <DropdownMenuItem key={link.href}>
                 <Link href={link.href} className="capitalize w-full">
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,21 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+import { NextResponse } from 'next/server';
+
+
 const isPublicRoute = createRouteMatcher(["/", "/products(.*)", "/about"]);
 //public has access to these routes
 
+const isAdminRoute = createRouteMatcher(['/admin(.*)'])
+//admin access only for these routes
+
 export default clerkMiddleware((auth, req) => {
+  const isAdminUser = auth().userId === process.env.ADMIN_USER_ID;
+
+  if (isAdminRoute(req) && !isAdminUser) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
   if (!isPublicRoute(req)) auth().protect();
 });
 //all other routes are protected
