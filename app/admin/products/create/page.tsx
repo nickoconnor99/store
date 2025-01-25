fbc0dbd712d9209bdc94723ea6af4cdaada383e3
Paul Allen <nickoconnor@gmail.com>
2025-01-25 12:30:52 +0000

    added form components

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,8 +1,36 @@
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { faker } from "@faker-js/faker";
+import FormInput from "@/components/form/FormInput";
+
+const createProductAction = async (formData: FormData) => {
+  "use server";
+  const name = formData.get("name") as string;
+  console.log(name);
+};
 
 function CreateProductPage() {
+  const name = faker.commerce.productName();
+  const company = faker.company.name();
+  const description = faker.lorem.paragraph({ min: 10, max: 12 });
   return (
-    <div>CreateProductPage</div>
-  )
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">create product</h1>
+      <div className="border p-8 rounded-md">
+        <form action={createProductAction}>
+          <FormInput
+            type="text"
+            name="name"
+            label="product name"
+            defaultValue={name}
+          ></FormInput>
+          <Button type="submit" size="lg">
+            Submit
+          </Button>
+        </form>
+      </div>
+    </section>
+  );
 }
-
-export default CreateProductPage
+export default CreateProductPage;
---
+++ components/form/Buttons.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { ReloadIcon } from '@radix-ui/react-icons';
+import { useFormStatus } from 'react-dom'; 
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { SignInButton } from '@clerk/nextjs';
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
+import { LuTrash2, LuPenSquare } from 'react-icons/lu';
+
+type btnSize = 'default' | 'lg' | 'sm';
+
+type SubmitButtonProps = {
+  className?: string;
+  text?: string;
+  size?: btnSize;
+};
+
+export function SubmitButton({
+  className = '',
+  text = 'submit',
+  size = 'lg',
+}: SubmitButtonProps) {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      type='submit'
+      disabled={pending}
+      className={cn('capitalize', className)}
+      size={size}
+    >
+      {pending ? (
+        <>
+          <ReloadIcon className='mr-2 h-4 w-4 animate-spin' />
+          Please wait...
+        </>
+      ) : (
+        text
+      )}
+    </Button>
+  );
+}
+
+//useFormStatus tells if pending is taking place, use pending value to disable button
