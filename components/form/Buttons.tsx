1f41c7bb4c0ceeeeb9c94e8ecab6403133f88974
Paul Allen <nickoconnor@gmail.com>
2025-01-25 12:33:57 +0000

    added form components

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -4,9 +4,9 @@ import { ReloadIcon } from '@radix-ui/react-icons';
 import { useFormStatus } from 'react-dom'; 
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
-import { SignInButton } from '@clerk/nextjs';
-import { FaRegHeart, FaHeart } from 'react-icons/fa';
-import { LuTrash2, LuPenSquare } from 'react-icons/lu';
+// import { SignInButton } from '@clerk/nextjs';
+// import { FaRegHeart, FaHeart } from 'react-icons/fa';
+// import { LuTrash2, LuPenSquare } from 'react-icons/lu';
 
 type btnSize = 'default' | 'lg' | 'sm';
 
