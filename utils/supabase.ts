fe2c600c06296231f97927cf7d1c0c4f32997484
Paul Allen <nickoconnor@gmail.com>
2025-01-26 12:30:18 +0000

    add updated .env

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -4,8 +4,8 @@ const bucket = "main-bucket";
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  process.env.SUPABASE_URL as string,
-  process.env.SUPABASE_KEY as string
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY as string
 );
 
 //getting image file from validaltefromZodSchema(imageSchema)
